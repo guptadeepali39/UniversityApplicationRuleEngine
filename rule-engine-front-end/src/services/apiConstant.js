0b66694ea2fdb3093ce35c7459e50d530a8cfea1
Mridul Khanna <mridulkhanna@Mriduls-MacBook-Pro.local>
2022-12-04 18:34:55 -0500

    api constant changed

--- rule-engine-front-end/src/services/apiConstant.js
+++ rule-engine-front-end/src/services/apiConstant.js
@@ -1,6 +1,6 @@
 const AppConstant = {
-  BASE_API_URL: "http://3.94.111.132:8002/api/",
-  ELIGIBILITY_API_URL: "http://3.94.111.132:8081/api/", // Staging
+  BASE_API_URL: "http://54.90.109.251:8002/api/",
+  ELIGIBILITY_API_URL: "http://54.90.109.251:8081/api/", // Staging
   // BASE_API_URL: "", // Production
   // BASE_API_URL: "http://localhost:8082/api/", // Localhost
 };
