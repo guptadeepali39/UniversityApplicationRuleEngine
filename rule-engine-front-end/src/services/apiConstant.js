80c8c4858fdf3f009545524d933be4df685a2b03
Mridul Khanna <mridulkhanna@Mriduls-MacBook-Pro.local>
2022-12-04 17:15:09 -0500

    changed ip address for api

--- rule-engine-front-end/src/services/apiConstant.js
+++ rule-engine-front-end/src/services/apiConstant.js
@@ -1,6 +1,6 @@
 const AppConstant = {
-  BASE_API_URL: "http://18.212.217.129:8002/api/",
-  ELIGIBILITY_API_URL: "http://18.212.217.129:8081/api/", // Staging
+  BASE_API_URL: "http://3.94.111.132:8002/api/",
+  ELIGIBILITY_API_URL: "http://3.94.111.132:8081/api/", // Staging
   // BASE_API_URL: "", // Production
   // BASE_API_URL: "http://localhost:8082/api/", // Localhost
 };
