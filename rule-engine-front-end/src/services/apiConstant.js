98d5f8b115642422df725f85ab2e9e6692e4bd12
Deepali Gupta <63843149+guptadeepali39@users.noreply.github.com>
2022-12-05 15:58:23 -0500

    Update apiConstant.js

--- rule-engine-front-end/src/services/apiConstant.js
+++ rule-engine-front-end/src/services/apiConstant.js
@@ -1,6 +1,6 @@
 const AppConstant = {
   BASE_API_URL:
-    "abea1adcbc1c74927bc4313a814e6a0b-1414301405.us-east-1.elb.amazonaws.com:8002/api/",
+    "http://abea1adcbc1c74927bc4313a814e6a0b-1414301405.us-east-1.elb.amazonaws.com:8002/api/",
   ELIGIBILITY_API_URL:
     "http://affa013f30dc14cd992ff5b6567d46c8-796613559.us-east-1.elb.amazonaws.com:8081/api/", // Staging
   // BASE_API_URL: "", // Production
