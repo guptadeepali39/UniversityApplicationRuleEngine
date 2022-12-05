45badc84b90e76f5ccf28e424b8fd3c8fdf1ba40
Mridul Khanna <mridulkhanna@Mriduls-MacBook-Pro.local>
2022-12-05 14:29:18 -0500

    api endpoints modified

--- rule-engine-front-end/src/services/apiConstant.js
+++ rule-engine-front-end/src/services/apiConstant.js
@@ -1,6 +1,8 @@
 const AppConstant = {
-  BASE_API_URL: "http://54.90.109.251:8002/api/",
-  ELIGIBILITY_API_URL: "http://54.90.109.251:8081/api/", // Staging
+  BASE_API_URL:
+    "abea1adcbc1c74927bc4313a814e6a0b-1414301405.us-east-1.elb.amazonaws.com:8002/api/",
+  ELIGIBILITY_API_URL:
+    "http://affa013f30dc14cd992ff5b6567d46c8-796613559.us-east-1.elb.amazonaws.com:8081/api/", // Staging
   // BASE_API_URL: "", // Production
   // BASE_API_URL: "http://localhost:8082/api/", // Localhost
 };
